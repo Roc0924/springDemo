9909920cabe7d943b63461e2fd5af0da3b797286
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-04-18 18:08:29 +0800

    print log via aop

---
+++ aspect_demo/src/test/java/com/example/Test.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2017/4/18
+ * Time                 : 18:08
+ * Description          :
+ */
+public class Test {
+}
