83a6df06426e90ac06e377fc746cf3cb095b591a
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-04-18 18:06:21 +0800

    print log via aop

---
+++ aspect_demo/src/main/java/com/example/dto/HelloDTO.java
@@ -0,0 +1,18 @@
+package com.example.dto;
+
+import lombok.Data;
+
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2017/4/18
+ * Time                 : 16:56
+ * Description          :
+ */
+@Data
+public class HelloDTO {
+    private String name;
+    private String apple;
+    private String banana;
+    private String orange;
+}
